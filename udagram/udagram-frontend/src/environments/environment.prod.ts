f27eabe3645eaca85e2d01d64c9bcc5a722c193f
DoanDuong99hust <doanduong1999@gmail.com>
2024-11-05 15:21:16 +0700

    edit api host env

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'uda-fullstack-default-app-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://uda-fullstack-default-app-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'uda-fullstack-default-app-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://uda-fullstack-default-app-dev.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
